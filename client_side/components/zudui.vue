256bd23fe40580612621b0fc1d84fc653e3fbd54
hanzif1 <2656253581@qq.com>
2023-10-23 13:04:10 +0800

    123

--- client_side/components/zudui.vue
+++ client_side/components/zudui.vue
@@ -1,16 +1,26 @@
 <template>
 	<view class="all">
 		<view class="saishi_fenlei">
-			<view class="fenlei">理</view>
-			<view class="fenlei">工</view>
-			<view class="fenlei">农</view>
-			<view class="fenlei">医</view>
-			<view class="fenlei">文</view>
-			<view class="fenlei">史</view>
-			<view class="fenlei">哲</view>
-			<view class="fenlei">政</view>
-			<view class="fenlei">经</view>
-			<view class="fenlei">管</view>
+			<view class="fenlei" v-if="li_clicked==0" @click="clickli">理</view>
+			<view class="fenlei2" v-else @click="clickli">理</view>
+			<view class="fenlei" v-if="gong_clicked==0" @click="clickgong">工</view>
+			<view class="fenlei2" v-else @click="clickgong">工</view>
+			<view class="fenlei" v-if="nong_clicked==0" @click="clicknong">农</view>
+			<view class="fenlei2" v-else @click="clicknong">农</view>
+			<view class="fenlei" v-if="yi_clicked==0" @click="clickyi">医</view>
+			<view class="fenlei2" v-else @click="clickyi">医</view>
+			<view class="fenlei" v-if="wen_clicked==0" @click="clickwen">文</view>
+			<view class="fenlei2" v-else @click="clickwen">文</view>
+			<view class="fenlei" v-if="shi_clicked==0" @click="clickshi">史</view>
+			<view class="fenlei2" v-else @click="clickshi">史</view>
+			<view class="fenlei" v-if="zhe_clicked==0" @click="clickzhe">哲</view>
+			<view class="fenlei2" v-else @click="clickzhe">哲</view>
+			<view class="fenlei" v-if="zheng_clicked==0" @click="clickzheng">政</view>
+			<view class="fenlei2" v-else @click="clickzheng">政</view>
+			<view class="fenlei" v-if="jing_clicked==0" @click="clickjing">经</view>
+			<view class="fenlei2" v-else @click="clickjing">经</view>
+			<view class="fenlei" v-if="guan_clicked==0" @click="clickguan">管</view>
+			<view class="fenlei2" v-else @click="clickguan">管</view>
 		</view>
 		<view class="detail">
 			<view class="detail_left">
@@ -56,6 +66,16 @@
 				xihuan_left:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 				shoucang_right:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 				xihuan_right:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+				li_clicked:0,
+				gong_clicked:0,
+				nong_clicked:0,
+				yi_clicked:0,
+				wen_clicked:0,
+				shi_clicked:0,
+				zhe_clicked:0,
+				zheng_clicked:0,
+				jing_clicked:0,
+				guan_clicked:0
 			}
 		},
 		onLoad() {
@@ -78,7 +98,36 @@
 				this.xihuan_right[x]=!this.xihuan_right[x];
 				this.$forceUpdate();
 			},
-			
+			clickli(){
+				this.li_clicked=!this.li_clicked
+			},
+			clickgong(){
+				this.gong_clicked=!this.gong_clicked
+			},
+			clicknong(){
+				this.nong_clicked=!this.nong_clicked
+			},
+			clickyi(){
+				this.yi_clicked=!this.yi_clicked
+			},
+			clickwen(){
+				this.wen_clicked=!this.wen_clicked
+			},
+			clickshi(){
+				this.shi_clicked=!this.shi_clicked
+			},
+			clickzhe(){
+				this.zhe_clicked=!this.zhe_clicked
+			},
+			clickzheng(){
+				this.zheng_clicked=!this.zheng_clicked
+			},
+			clickjing(){
+				this.jing_clicked=!this.jing_clicked
+			},
+			clickguan(){
+				this.guan_clicked=!this.guan_clicked
+			}
 		}
 	}
 </script>
@@ -103,24 +152,37 @@
 			border-radius: 20rpx;
 			/* box-shadow: 0px 0px 50px 0px rgba(0,0,0,0.15); */
 		}
-		.fenlei{
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			background-color: #ffffff;
-			font-size: 50rpx;
-			height: 5vh;
-			width:16vw;
-			font-family: "黑体";
-			color: rgb(52,120,246);
-			font-size: 3vh;
-			border-radius: 2.5vh;
-	/* 		border-style: solid;
-			border-color: grey;
-			border-width: 1rpx; */
-			box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
-		}
-	
+	.fenlei{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: #ffffff;
+		font-size: 50rpx;
+		height: 5vh;
+		width:16vw;
+		font-family: "黑体";
+		color: rgb(52,120,246);
+		font-size: 3vh;
+		border-radius: 2.5vh;
+		box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+	}
+	.fenlei2{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: rgb(52,120,246);
+		font-size: 50rpx;
+		height: 5vh;
+		width:16vw;
+		font-family: "黑体";
+		color: #ffffff;
+		font-size: 3vh;
+		border-radius: 2.5vh;
+/* 		border-style: solid;
+		border-color: grey;
+		border-width: 1rpx; */
+		box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+	}
 	.detail{
 		display: flex;
 		margin-top:2vh;
---
+++ client_side/pages/index/fatie.vue
@@ -0,0 +1,163 @@
+<template>
+	<view class="all">
+		<view class="title">发帖子</view>
+		<view class="yonghu-biaoti">
+			<view class="user">
+				<image class="touxiang" src="../../static/logo.png"></image>
+				<view class="userid">用户名</view>
+			</view>
+			<input class="biaoti" placeholder="标题"></input>
+		</view>
+		<view class="neirong">
+			<textarea class="neirong-text" placeholder="内容"></textarea>
+		</view>
+		<view class="jiaonang">
+			<view class="huati-yonghu">#话题</view>
+			<view class="huati-yonghu">@用户</view>
+			<image class="add" src="../../static/add.png"></image>
+		</view>
+		<view class="gongkai_or_simi">
+			<image class="simi" src="../../static/simi.png"></image>
+			<view class="simi-text">私密</view>
+			<switch class="switch" :checked="false" style="transform:scale(0.8);"></switch>
+		</view>
+		<view class="bottom">
+			<image class="cuncaogao" src="../../static/cuncaogao.png">保存草稿</image>
+			<button class="fabu">发布</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style>
+	.all{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.title{
+		display: flex;
+		/* background-color: aqua; */
+		height: 8vh;
+		width: 90vw;
+		justify-content: center;
+		align-items: center;
+		font-size: 35rpx;
+		border-bottom-style: ridge;
+	}
+	.yonghu-biaoti{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		/* background-color: aquamarine; */
+		width: 90vw;
+		height: 18vh;
+		border-bottom-style: ridge;
+	}
+	.user{
+		display: flex;
+		align-items: center;
+		height: 9vh;
+		margin-left: 2vw;
+	}
+	.touxiang{
+		width: 80rpx;
+		height: 80rpx;
+		border-radius: 50%;
+	}
+	.biaoti{
+		font-size: 50rpx;
+		margin-left: 2vw;
+		margin-bottom: 1vh;
+	}
+	.neirong-text{
+		display: flex;
+		flex-wrap: wrap;
+		width: 85vw;
+		height: 30vh;
+		margin-top: 1vh;
+		margin-bottom: 1vh;
+		/* background-color: antiquewhite; */
+	}
+	.jiaonang{
+		display: flex;
+		width: 90vw;
+	}
+	.huati-yonghu{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: #ffffff;
+		font-size: 50rpx;
+		height: 5vh;
+		width:16vw;
+		font-family: "黑体";
+		color: rgb(52,120,246);
+		font-size: 3vh;
+		border-radius: 20rpx;
+		box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+		margin-left: 3vw;
+	}
+	.add{
+		border-radius: 50%;
+		margin-left: 5vw;
+		width: 65rpx;
+		height: 65rpx;
+	}
+	.gongkai_or_simi{
+		display: flex;
+		align-items: center;
+		/* background-color: aliceblue; */
+		width: 90vw;
+		height: 7vh;
+		margin-top: 3vh;
+		border-top-style: ridge;
+		border-bottom-style: ridge;
+	}
+	.simi{
+		margin-left: 2vw;
+		width: 50rpx;
+		height: 50rpx;
+	}
+	.simi-text{
+		margin-left: 2vw;
+	}
+	.switch{
+		margin-left: 55vw;
+	}
+	.bottom{
+		display: flex;
+		align-items: center;
+		position: fixed;
+		bottom:2vh;
+		width: 90vw;
+		height: 8vh;
+		background-color: aliceblue;
+	}
+	.cuncaogao{
+		margin-left: 2vh;
+		margin-right: 1vh;
+		width:60rpx;
+		height: 60rpx;	
+	}
+	.fabu{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 50vw;
+		height: 5vh;
+		border-radius: 50rpx;
+		background-color: rgb(52,120,245);
+		color:white;
+		box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+	}
+</style>
