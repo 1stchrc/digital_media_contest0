56a564bb7702afa99c10f1dac144ef40b5764775
hanzif1 <2656253581@qq.com>
2023-10-25 21:20:08 +0800

    a

--- client_side/pages/index/zudui_tiezi.vue
+++ client_side/pages/index/zudui_tiezi.vue
@@ -6,21 +6,33 @@
 			<view class="user_guanzhu" v-if="guanzhu==0" @click="guanzhu1()">关注</view>
 			<view class="user_guanzhu" v-else @click="guanzhu1()">已关注</view>
 		</view>
-		<view class="zhengwen">
-			<view class="title">
-				这里是题目
+		<view class="bisai-zhaomu">
+			* * * 比 赛 招 募
+		</view>
+		<view class="zhiwei-jinengmiaoshu">
+			<view class="zhiwei-jineng-detail">
+				<uni-section title="多选" type="line">
+					<view class="uni-px-5 uni-pb-5">
+						<uni-data-checkbox class="zhiwei" multiple v-model="checkbox1" :localdata="hobby"></uni-data-checkbox>
+					</view>
+				</uni-section>
+				<view class="jineng">技能描述</view>
 			</view>
-			<view class="detail">
-				这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文这里是正文
+			<view class="zhiwei-jineng-detail">
+				<uni-section title="多选" type="line">
+					<view class="uni-px-5 uni-pb-5">
+						<uni-data-checkbox class="zhiwei" multiple v-model="checkbox1" :localdata="hobby"></uni-data-checkbox>
+					</view>
+				</uni-section>
+				<view class="jineng">技能描述</view>
 			</view>
-			<image class="image" src="../../static/add.png"></image>
 		</view>
 		<view class="bottom">
-			<input class="pinglun" placeholder="评论"></input>
 			<image class="dianzan" v-if="dianzan==0" @click="dianzan1()" src="../../static/xihuan.png"></image>
 			<image class="dianzan" v-else @click="dianzan1()" src="../../static/xihuan_selected.png"></image>
 			<image class="shoucang" v-if="shoucang==0" @click="shoucang1()" src="../../static/shoucang.png"></image>
 			<image class="shoucang" v-else @click="shoucang1()" src="../../static/shoucang_selected.png"></image>
+			<view class="toudi">投递简历</view>
 		</view>
 	</view>
 </template>
@@ -31,7 +43,11 @@
 			return{
 				guanzhu:false,
 				dianzan:false,
-				shoucang:false
+				shoucang:false,
+				hobby: [{
+					text: '职位',
+					value: 0
+				}],
 			}
 		},
 		methods:{
@@ -49,6 +65,27 @@
 </script>
 
 <style>
+	.jineng{
+		
+		color: #666;
+		font-size: 25px;
+		margin-left: 9vw;
+	}
+	.zhiwei{
+		width: 30vw;
+		height: 5vh;
+		/* background-color: aliceblue */
+	}
+	.bisai-zhaomu{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		/* background-color: aqua; */
+		width: 90vw;
+		height: 6vh;
+		font-size: 50rpx;
+		margin-top: 12vh;
+	}
 	.all{
 		display: flex;
 		flex-direction: column;
@@ -79,31 +116,6 @@
 		width: 18vw;
 		height: 4.5vh;
 	}
-	.zhengwen{
-		margin-top: 9vh;
-		width: 90vw;
-		padding-bottom: 3vh;
-		overflow-y: auto;
-		/* background-color: aquamarine; */
-	}
-	.title{
-		/* background-color: aqua; */
-		margin-top: 3vh;
-		margin-left: 1vw;
-		width: 89vw;
-		height: 5vh;
-		font-size: 50rpx;
-	}
-	.detail{
-		margin-top: 5vh;
-		margin-left: 1vw;
-		margin-right: 1vw;
-	}
-	.image{
-		width: 90vw;
-		margin-top: 1vh;
-		margin-bottom: 1vh;
-	}
 	.bottom{
 		display: flex;
 		align-items: center;
@@ -114,24 +126,46 @@
 		background-color: white;
 		border-top-style: ridge;
 	}
-	.pinglun{
-		width: 60vw;
-		border-style: solid;
-		border-radius: 30rpx;
-		border-color: rgb(154,154,154);
-		padding-left: 3vw;
-		border-width: 5rpx;
-		height: 4vh;
-	}
 	.dianzan,.shoucang{
 		width: 50rpx;
 		height: 50rpx;
 	}
 	.dianzan{
-		margin-left: 5vw;
+		margin-left: 3vw;
 	}
 	.shoucang{
-		margin-left: 4vw;
+		margin-left: 5vw;
 		margin-bottom: 0.3vh;
+	}
+	.toudi{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+		margin-left: 15vw;
+		border-radius: 30rpx;
+		background-color: rgb(52,120,245);
+		width: 50vw;
+		height: 5vh;
+		box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+		
+	}
+	.zhiwei-jinengmiaoshu{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center;
+		/* background-color: aqua; */
+		width: 90vw;
+		height: 50vh;
+	}
+	.zhiwei-jineng-detail{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		/* background-color: antiquewhite; */
+		width: 88vw;
+		height: 20vh;
+		
 	}
 </style>
